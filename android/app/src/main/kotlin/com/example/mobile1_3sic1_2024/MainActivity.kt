14c97e4bfc01e0bd711677df3658ad41028a54ee
Azlan-saja <azlansaja19@gmail.com>
2024-11-13 19:18:06 +0700

    first

---
+++ android/app/src/main/kotlin/com/example/mobile1_3sic1_2024/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.mobile1_3sic1_2024
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Home'),
+        ),
+        body: const Text('ini adalah body'),
+        bottomNavigationBar: BottomNavigationBar(
+          items: const [
+            BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.home,
+                ),
+                label: 'home'),
+            BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.people,
+                ),
+                label: 'user'),
+            BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.logout,
+                ),
+                label: 'keluar'),
+          ],
+        ),
+      ),
+    );
+  }
+}
